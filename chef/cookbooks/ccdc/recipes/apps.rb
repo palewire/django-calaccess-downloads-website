640ec492568de2b57e682588c5be5a3ad3d9fb3a
James Gordon <gordon.je@gmail.com>
2016-06-06 15:24:23 -0500

    Add syncing with github repo

--- chef/cookbooks/ccdc/recipes/apps.rb
+++ chef/cookbooks/ccdc/recipes/apps.rb
@@ -19,37 +19,12 @@ directory "/apps/calaccess/repo" do
     mode 0775
 end
 
-remote_directory "/apps/calaccess/repo" do
-  files_mode '0777'
-  files_owner node[:app_user]
-  mode '0777'
-  owner node[:app_user]
-  source "django"
-end
-
-# Install the virtualenv requirements
-script "Install requirements" do
-  interpreter "bash"
+# Pull the git repo
+git "/apps/#{node[:app][:name]}/repo"  do
+  repository node[:app][:repo]
+  reference "HEAD"
+  revision node[:app][:branch]
   user node[:apps_user]
   group node[:apps_group]
-  code "/apps/calaccess/bin/pip install -r /apps/calaccess/repo/requirements.txt"
-end
-
-# Create the database user
-script "Create database user" do
-  interpreter "bash"
-  user node[:apps_user]
-  code <<-EOH
-     mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO #{node[:db_user]}@'%' IDENTIFIED BY '#{node[:db_password]}'"
-  EOH
-  ignore_failure true
-end
-
-# Create the database
-script "Create database" do
-  interpreter "bash"
-  user "ubuntu"
-  code <<-EOH
-    mysql -u #{node[:db_user]} -p#{node[:db_password]} -e "create database #{node[:db_name]}"; 
-  EOH
+  action :sync
 end
