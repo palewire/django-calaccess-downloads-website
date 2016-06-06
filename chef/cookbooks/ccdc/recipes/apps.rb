fd6af12e5bce640d6705e3cd786b94d9cde4a533
Ben Welsh <ben.welsh@gmail.com>
2016-06-06 09:21:49 -0700

    Update apps.rb

--- chef/cookbooks/ccdc/recipes/apps.rb
+++ chef/cookbooks/ccdc/recipes/apps.rb
@@ -34,22 +34,3 @@ script "Install requirements" do
   group node[:apps_group]
   code "/apps/calaccess/bin/pip install -r /apps/calaccess/repo/requirements.txt"
 end
-
-# Create the database user
-script "Create database user" do
-  interpreter "bash"
-  user node[:apps_user]
-  code <<-EOH
-     mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO #{node[:db_user]}@'%' IDENTIFIED BY '#{node[:db_password]}'"
-  EOH
-  ignore_failure true
-end
-
-# Create the database
-script "Create database" do
-  interpreter "bash"
-  user "ubuntu"
-  code <<-EOH
-    mysql -u #{node[:db_user]} -p#{node[:db_password]} -e "create database #{node[:db_name]}"; 
-  EOH
-end
