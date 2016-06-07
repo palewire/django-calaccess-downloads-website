9dfdd431776fb7cb7c28cceb8bf847f96f772899
James Gordon <gordon.je@gmail.com>
2016-06-07 14:47:23 -0500

    Switch hard-coded db settings to env vars

--- cacivicdata/cacivicdata/settings.py
+++ cacivicdata/cacivicdata/settings.py
@@ -65,14 +65,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cacivicdata.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'calaccess_raw',
+        'USER': 'cacivicdata',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '5432'
+    },
 }
 
 
