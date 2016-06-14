e28f4d11b35a39363489348967093945a5394435
palewire <ben.welsh@gmail.com>
2016-06-13 21:34:18 -0700

    Removed pip packages bit since we're not using it

--- chef/cookbooks/ccdc/recipes/python.rb
+++ chef/cookbooks/ccdc/recipes/python.rb
@@ -4,11 +4,3 @@ node[:ubuntu_python_packages].each do |pkg|
         :upgrade
     end
 end
-
-# System wide python packages via pip
-node[:pip_python_packages].each_pair do |pkg, version|
-    execute "install-#{pkg}" do
-        command "pip install #{pkg}==#{version}"
-        not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
-    end
-end
