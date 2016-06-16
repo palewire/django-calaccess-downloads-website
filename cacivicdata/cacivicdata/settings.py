1461941560da941d784b54d16e80e0538345124c
James Gordon <gordon.je@gmail.com>
2016-06-16 09:02:02 -0500

    Allow searching for templates in app dir (default)

--- cacivicdata/cacivicdata/settings.py
+++ cacivicdata/cacivicdata/settings.py
@@ -50,7 +50,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['cacivicdata/templates/'],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
