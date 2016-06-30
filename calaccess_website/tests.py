08f938a7ad4ea7778c71c921c1f240ccb0206778
palewire <ben.welsh@gmail.com>
2016-06-30 09:38:32 -0700

    first real test using the build command surfaces the 'empty database' bug encountered by @sahilchinoy

--- calaccess_website/tests.py
+++ calaccess_website/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.core import management
 
 
 class WebsiteTest(TestCase):
 
-    def test_foo(self):
-        self.assertEqual(2 + 2, 4)
+    def test_build(self):
+        management.call_command("build")
