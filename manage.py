1512bc42745341eb84dad23ca4d179a0268a3c16
James Gordon <gordon.je@gmail.com>
2016-07-18 13:19:31 -0500

    Replace dotenv with ConfigParser

--- manage.py
+++ manage.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 import os
 import sys
-import dotenv
+import ConfigParser
 
 if __name__ == "__main__":
-    repo_dir = os.path.dirname(os.path.realpath(__file__))
-    dotenv.read_dotenv(os.path.join(repo_dir, ".env"))
+    # load env variables from .env config file
+    cp = ConfigParser.SafeConfigParser()
+    cp.read('.env')
+    # default to DEV env
+    os.environ.setdefault("CALACCESS_WEBSITE_ENV", "DEV")
+    cp_sect = os.getenv("CALACCESS_WEBSITE_ENV").upper()
+
+    if not cp.has_section(cp_sect):
+        raise EnvironmentError("No [{0}] section found in .env file (run "
+            "`fab createconfig).".format(cp_sect))
+
+    for i in cp.items(os.getenv("CALACCESS_WEBSITE_ENV").upper()):
+        os.environ[i[0]] = i[1]
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
