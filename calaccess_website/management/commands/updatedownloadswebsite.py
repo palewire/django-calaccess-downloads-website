14c97deb95d20396e13809a536361051a886f1df
James Gordon <gordon.je@gmail.com>
2016-07-22 14:05:52 -0500

    Rename update website command, add publish opt

---
+++ calaccess_website/management/commands/updatedownloadswebsite.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Update to the latest CAL-ACCESS snapshot and bake static website pages.
+"""
+import logging
+from django.core.management import call_command
+from calaccess_raw.management.commands.updatecalaccessrawdata import Command as updatecommand
+logger = logging.getLogger(__name__)
+
+
+class Command(updatecommand):
+    """
+    Update to the latest CAL-ACCESS snapshot and bake static website pages.
+    """
+    help = 'Update to the latest CAL-ACCESS snapshot and bake static website pages'
+
+    def add_arguments(self, parser):
+        """
+        Adds custom arguments specific to this command.
+        """
+        super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            "--publish",
+            action="store_true",
+            dest="keep_files",
+            default=False,
+            help="Publish baked content"
+        )
+
+    def handle(self, *args, **options):
+        """
+        Make it happen.
+        """
+        super(Command, self).handle(*args, **options)
+
+        self.header('Creating latest file links')
+        call_command('createlatestlinks')
+        self.header('Baking downloads-website content')
+        call_command('build')
+        if options['publish']:
+            self.header('Publishing backed content to S3 bucket.')
+            call_command('publish')
+
+        self.success("Done!")
