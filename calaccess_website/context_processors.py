8893d5e601773b751966aa401a75e1f03d4063ed
palewire <ben.welsh@gmail.com>
2016-07-23 17:48:54 -0700

    Added description to context processor

--- calaccess_website/context_processors.py
+++ calaccess_website/context_processors.py
@@ -3,5 +3,7 @@ def calaccess_website(request):
     Custom context variables for the CAL-ACCESS downloads website.
     """
     return {
-        'CALACCESS_WEBSITE_TITLE': 'The CAL-ACCESS archive'
+        'CALACCESS_WEBSITE_TITLE': 'The CAL-ACCESS archive',
+        'CALACCESS_WEBSITE_DESCRIPTION': "Daily snapshots of campaign finance \
+and lobbying disclosure data from California",
     }
