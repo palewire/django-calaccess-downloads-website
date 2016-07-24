939a055e2cdba1e7a536ef392ba4cf2d44e234e0
palewire <ben.welsh@gmail.com>
2016-07-23 17:46:51 -0700

    Created a context processor for this app to store some of our common labels and such

---
+++ calaccess_website/context_processors.py
@@ -0,0 +1,7 @@
+def calaccess_website(request):
+    """
+    Custom context variables for the CAL-ACCESS downloads website.
+    """
+    return {
+        'CALACCESS_WEBSITE_TITLE': 'The CAL-ACCESS archive'
+    }
