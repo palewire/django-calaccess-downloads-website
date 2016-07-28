63e969b5fa4720a0ea09ea1eb982ab0cf3a83b85
James Gordon <gordon.je@gmail.com>
2016-07-28 09:43:05 -0500

    Switch to boto3, fix issue with not getting latest update

--- calaccess_website/management/commands/createlatestlinks.py
+++ calaccess_website/management/commands/createlatestlinks.py
@@ -7,7 +7,7 @@ directory in the Django project's default file storage.
 import os
 import logging
 from django.conf import settings
-from boto.s3.connection import S3Connection, OrdinaryCallingFormat
+import boto3
 from calaccess_raw.models import RawDataCommand
 from django.core.management.base import BaseCommand
 logger = logging.getLogger(__name__)
@@ -22,9 +22,18 @@ class Command(BaseCommand):
 in a latest directory in the Django project's default file storage."
 
     def handle(self, *args, **options):
-        # get the version of the most last completed update
+        # set up boto session
+        self.session = boto3.Session(
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            region_name=settings.AWS_S3_REGION_NAME
+        )
+        # and client
+        self.client = self.session.client('s3')
+
+        # get the version of last update that finished
         v = RawDataCommand.objects.filter(
-            command='updatecalaccessrawdata',
+            command__contains='update',
             finish_datetime__isnull=False
         ).latest('version__release_datetime').version
 
@@ -34,38 +43,21 @@ in a latest directory in the Django project's default file storage."
             )
         )
 
-        # convert the zip files path to the latest path
-        latest_zip_path = self.get_latest_path(v.download_zip_archive.name)
-
         # save zip to the latest directory
-        logger.debug('Saving copy of zip file')
-        self.copy_key(latest_zip_path, v.download_zip_archive.name)
+        self.copy_to_latest(v.download_zip_archive.name)
 
         # loop through all of the raw data files
         for f in v.files.all():
-            # convert the downloaded file's path to the latest path
-            latest_dl_path = self.get_latest_path(f.download_file_archive.name)
             # save downloaded file to the latest directory
-            logger.debug('Saving copy of {0}.TSV'.format(f.file_name))
-            self.copy_key(latest_dl_path, f.download_file_archive.name)
+            self.copy_to_latest(f.download_file_archive.name)
 
             if f.clean_file_archive:
-                # convert the cleaned file's path to the latest path
-                latest_cl_path = self.get_latest_path(f.clean_file_archive.name)
                 # save cleaned file to the latest directory
-                logger.debug('Saving copy of {0}.csv'.format(
-                    f.file_name.lower()
-                ))
-                self.copy_key(latest_cl_path, f.clean_file_archive.name)
+                self.copy_to_latest(f.clean_file_archive.name)
 
             if f.error_log_archive:
-                # convert the error log file's path to the latest path
-                latest_el_path = self.get_latest_path(f.error_log_archive.name)
                 # save error log file to the latest directory
-                logger.debug('Saving copy of {0}.errors.csv'.format(
-                    f.file_name.lower()
-                ))
-                self.copy_key(latest_el_path, f.error_log_archive.name)
+                self.copy_to_latest(f.error_log_archive.name)
 
     def get_latest_path(self, old_path):
         """
@@ -86,14 +78,16 @@ in a latest directory in the Django project's default file storage."
         )
         return conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
 
-    def copy_key(self, target, source):
+    def copy_to_latest(self, source):
         """
         Copies the provided source key to the provided target key
         """
-        bucket = self.get_bucket()
-        bucket.copy_key(
-            target,
-            settings.AWS_STORAGE_BUCKET_NAME,
-            source,
-            preserve_acl=True,
+        logger.debug('Saving copy of %s' %os.path.basename(source))
+        self.client.copy_object(
+            Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+            Key=self.get_latest_path(source),
+            CopySource={
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
+                'Key': source,
+            },
         )
