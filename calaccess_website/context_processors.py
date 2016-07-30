d66ca974bd006a836d37e0522c6c2606f27ee34b
palewire <ben.welsh@gmail.com>
2016-07-30 15:29:46 -0700

    Filled in the context_processor with our global website details

--- calaccess_website/context_processors.py
+++ calaccess_website/context_processors.py
@@ -3,8 +3,8 @@ def calaccess_website(request):
     Custom context variables for the CAL-ACCESS downloads website.
     """
     return {
-        'CALACCESS_WEBSITE_TITLE': 'California campaign data',
-        'CALACCESS_WEBSITE_DESCRIPTION': "Download campaign finance and lobbying disclosure data from \
-the California Secretary of State's CAL-ACCESS database ",
+        'CALACCESS_WEBSITE_TITLE': 'California Civic Data Coalition',
+        'CALACCESS_WEBSITE_DESCRIPTION': "An open-source team of journalists \
+and computer programmers from news organizations across America. ",
         'CALACCESS_WEBSITE_DOMAIN': 'calaccess.californiacivicdata.org'
     }
