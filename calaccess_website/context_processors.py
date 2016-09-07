687edeea775c9fdd09a760f7c10f59fb8817c801
palewire <ben.welsh@gmail.com>
2016-09-07 12:47:35 -0700

    Dummy up the head

--- calaccess_website/context_processors.py
+++ calaccess_website/context_processors.py
@@ -1,9 +1,12 @@
+# from greeking.lorem
+
+
 def calaccess_website(request):
     """
     Custom context variables for the CAL-ACCESS downloads website.
     """
     return {
-        'CALACCESS_WEBSITE_TITLE': 'California Campaign Finance Data Refinery',
+        'CALACCESS_WEBSITE_TITLE': 'HEADLINE GOES HERE',
         'CALACCESS_WEBSITE_DESCRIPTION': "Download ready-to-use California \
         campaign finance and lobbying data. We've made the data \
         understandable with documentation and examples. Brought to you by the\
