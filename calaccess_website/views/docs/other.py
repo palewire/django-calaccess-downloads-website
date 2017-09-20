657e79234a00294d8156deeaddb961404b73e361
palewire <ben.welsh@gmail.com>
2017-09-19 17:57:36 -0700

    More refinement of the documentation index

--- calaccess_website/views/docs/other.py
+++ calaccess_website/views/docs/other.py
@@ -16,35 +16,35 @@ class DocumentationIndex(BuildableTemplateView):
         form_list = all_filing_forms
         object_list = [
             dict(
-                name='Processed civic data files',
+                name='Processed files from the California Civic Data Coalition',
                 description="Definitions, record layouts and data dictionaries for the \
-processed data files released by the California Civic Data Coalition. These files are recommended for beginners and regular use.",
+processed data files released by the California Civic Data Coalition. Recommended for beginners and regular use.",
                 url=reverse("ccdc_file_list"),
             ),
             dict(
-                name='Raw CAL-ACCESS files',
+                name='Raw files from CAL-ACCESS',
                 description="Definitions, record layouts and data dictionaries for the {} raw \
-files released from the California Secretary of State's CAL-ACCESS database. These files are for experts only.".format(len(model_list)),
+files released from the California Secretary of State's CAL-ACCESS database. For experts only.".format(len(model_list)),
                 url=reverse("calaccess_file_list"),
             ),
             dict(
-                name='CAL-ACCESS forms',
+                name='Official CAL-ACCESS forms',
                 description="Descriptions, samples and other documentation for \
 the {} forms that campaigns and lobbyists use to disclose activity to California's Secretary of State.".format(len(form_list)),
                 url=reverse("form_list"),
             ),
             dict(
-                name='CAL-ACCESS official documentation',
+                name='Official CAL-ACCESS documentation',
                 description="The jumbled, fragmentary and unreliable documentation \
 for the CAL-ACCESS database provided by California's Secretary of State. For more authoritative \
 information refer to the materials above.",
                 url=reverse("official_documentation"),
             ),
             dict(
-                name='Civic data technical documentation',
-                description="User manuals for the collection of \
+                name='Technical documentation',
+                description="A user manual for the collection of \
 California Civic Data Coalition applications that power this site. For developers only.",
-                url="http://django-calaccess.californiacivicdata.org"
+                url="//django-calaccess.californiacivicdata.org"
             ),
             dict(
                 name="Frequently asked questions",
