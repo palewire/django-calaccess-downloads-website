6011a4b097c4aaa6bdc5a44283ec402be380c895
palewire <ben.welsh@gmail.com>
2017-09-19 18:02:03 -0700

    Added share image url to the context processor

--- calaccess_website/context_processors.py
+++ calaccess_website/context_processors.py
@@ -9,5 +9,6 @@ def calaccess_website(request):
         'CALACCESS_WEBSITE_TITLE': 'California Civic Data Coalition',
         'CALACCESS_WEBSITE_DESCRIPTION': "An open-source team of journalists \
 and computer programmers from news organizations across America.",
-        'CALACCESS_WEBSITE_DOMAIN': 'calaccess.californiacivicdata.org'
+        'CALACCESS_WEBSITE_DOMAIN': 'calaccess.californiacivicdata.org',
+        'CALACCESS_WEBSITE_SHARE_IMG': '//calaccess.californiacivicdata.org/static/calaccess_website/images/brown-bear-share.png'
     }
