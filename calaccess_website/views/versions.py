b0d9ad47738f52c2d77bc22a05feab7f0ff6129a
palewire <ben.welsh@gmail.com>
2017-10-24 09:07:37 -0700

    Simplified latest template's meta tags

--- calaccess_website/views/versions.py
+++ calaccess_website/views/versions.py
@@ -183,6 +183,9 @@ class LatestVersion(VersionDetail):
         """
         context = super(LatestVersion, self).get_context_data(**kwargs)
         # A hint we can use in the template as a switch
+        context['title'] = 'Latest download'
+        context['description'] = 'The most recent release of CAL-ACCESS, the government database that tracks campaign \
+finance and lobbying activity in California politics.'
         context['is_latest'] = True
         return context
 
