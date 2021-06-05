23009620a7bc9a57e1065aaa1004ab90a27037ef
palewire <b@palewi.re>
2021-06-05 14:46:39 -0700

    A real clock

--- clock.py
+++ clock.py
@@ -2,14 +2,23 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', hour="*/3", minute=0)
+def updater():
+    """
+    Run our update command every three hours.
+    """
+    # Set env
     import os
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+    # Boot Django
     import django
     django.setup()
+
+    # Run the command
     from django.core.management import call_command
-    call_command("check")
+    call_command("updatedownloadswebsite", noinput=True, verbosity=2)
+
 
 sched.start()
